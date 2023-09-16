ca990f30f2ce3229a288139ed4be6f627dcb9a31
Valentinonai <137069763+Valentinonai@users.noreply.github.com>
2023-09-16 21:32:44 +0200

    v3.0

--- src/components/Forecasts.jsx
+++ src/components/Forecasts.jsx
@@ -26,7 +26,8 @@ const Forecasts = (props) => {
         <div className=" mt-5 gy-4" style={{ overflowX: "scroll" }}>
           {forecast.map(
             (elem, index) =>
-              timeConverter(elem.dt)[1] === timeConverter(forecast[0].dt)[1] && (
+              timeConverter(elem.dt)[1] === timeConverter(forecast[1].dt)[1] &&
+              timeConverter(elem.dt)[0] > props.today && (
                 <Row className="align-items-center singForecast">
                   <Col xs={3}>
                     <span className="pt-2">{timeConverter(elem.dt)[2]}</span>
